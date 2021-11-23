8d64557ecc934ce69fad3553240f6565e27227a5
Ben McLoughlin <benmcl@shaw.ca>
2021-11-23 07:26:32 -0800

    23 Nov 2021 - added Timer component, no timer counter yet

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Platform } from 'react-native';
 import { Focus } from './src/features/focus/Focus';
 import { Timer } from './src/features/timer/Timer';
-import { spacing } from '../../utils/sizes';
+import { spacing } from './src/utils/sizes';
 
 export default function App() {
     const [focusSubject, setFocusSubject] = useState(null);
     return (
         <View style={styles.container}>
-            <Text>{focusSubject}</Text>
-            <Focus addSubject={setFocusSubject} />
+            {focusSubject ? (
+                <Timer addSubject={setFocusSubject} />
+            ) : (
+                <Focus addSubject={setFocusSubject} />
+            )}
         </View>
     );
 }
--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,9 +1,14 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { spacing } from '../../utils/sizes';
+import { colors } from '../../utils/colors';
 
 export function Timer({ focusSubject }) {
     return (
         <View style={styles.container}>
+            <View style={{ paddingTop: spacing.xxl }}></View>
+            <Text style={styles.title}>Focusing on: </Text>
+            <Text style={styles.task}>{focusSubject}</Text>
             <Text>Timer Goes Here: {focusSubject}</Text>
         </View>
     );
@@ -14,4 +19,6 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 50,
     },
+    title: { color: colors.white, textAlign: 'center' },
+    task: { color: colors.white, fontWeight: 'bold' },
 });
